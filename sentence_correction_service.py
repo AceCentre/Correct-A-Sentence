07ab576fdb65938ce2ac14eaea35d45ab1bfb8c9
will wade <willwade@gmail.com>
2024-01-17 16:59:01 +0000

    fixing for do?

--- sentence_correction_service.py
+++ sentence_correction_service.py
@@ -53,4 +53,4 @@ class SentenceCorrection(Resource):
         return {'corrected_sentence': corrected_sentence}
 
 if __name__ == '__main__':
-    app.run(port=80, debug=False)
+    app.run(host='0.0.0.0', port=8080, debug=False)
