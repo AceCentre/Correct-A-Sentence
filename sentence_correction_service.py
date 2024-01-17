a622f9aeb01de3e94e0eb09e6c5a57641d75e569
will wade <willwade@gmail.com>
2024-01-17 14:19:02 +0000

    adding a option to correct

--- sentence_correction_service.py
+++ sentence_correction_service.py
@@ -14,18 +14,19 @@ api = Api(app, version='1.0', title='Sentence Correction API',
 ns = api.namespace('correction', description='Sentence Corrections')
 
 sentence_model = api.model('Sentence', {
-    'text': fields.String(required=True, description='The sentence to be corrected')
+    'text': fields.String(required=True, description='The sentence to be corrected'),
+    'correct_typos': fields.Boolean(required=False, default=True, description='Whether to correct typos')
 })
 
-def correct_sentence(input_string):
+def correct_sentence(input_string, correct_typos=True):
     # Segment the string into words
     segmented = segment(input_string)
 
     # Initialize spell checker
     spell = SpellChecker()
 
-    # Correct typos in each word
-    corrected_words = [spell.correction(word) for word in segmented]
+    # Correct typos in each word if correct_typos is True
+    corrected_words = [spell.correction(word) if correct_typos else word for word in segmented]
 
     # Case correction (basic)
     corrected_case = []
@@ -38,7 +39,6 @@ def correct_sentence(input_string):
     # Join the words into a sentence
     return ' '.join(corrected_case)
 
-
 @ns.route('/correct_sentence')
 class SentenceCorrection(Resource):
     @api.expect(sentence_model)
@@ -48,13 +48,9 @@ class SentenceCorrection(Resource):
         '''Correct and segment a sentence'''
         data = request.json
         input_string = data.get('text', '')
-        corrected_sentence = correct_sentence(input_string)
+        correct_typos = data.get('correct_typos', True)
+        corrected_sentence = correct_sentence(input_string, correct_typos)
         return {'corrected_sentence': corrected_sentence}
-
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
