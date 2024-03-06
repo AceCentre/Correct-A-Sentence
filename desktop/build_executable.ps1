40bf7fea7bd7c63240a55b7c6bb2bffe461e889b
will wade <willwade@gmail.com>
2024-03-06 22:12:08 +0000

    fix added n on upx line

--- desktop/build_executable.ps1
+++ desktop/build_executable.ps1
@@ -1,4 +1,4 @@
 cmd /c "python -m PyInstaller correctsentence_full.spec --noconfirm --upx-dir=C:\upx"
 cmd /c "python -m PyInstaller client.spec --noconfirm --upx-dir=C:\upx"
-cmd /c "python -m PyInstaller CreateGridset.py --noconsole --noconfirm --onedir --clean --upx-dir=C:\upxn"
+cmd /c "python -m PyInstaller CreateGridset.py --noconsole --noconfirm --onedir --clean --upx-dir=C:\upx"
 cmd /c '"C:\Program Files (x86)\NSIS\makensis.exe" .\buildscript.nsi'
